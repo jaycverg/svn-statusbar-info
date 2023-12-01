afbdfbc0ee206deca6a831da92f11513456a03f5
jayrome <jaycverg@gmail.com>
2023-12-02 01:06:20 +0800

    set dependency to Subversion plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ intellij {
   version.set("2023.1.5")
   type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf("Subversion"))
 }
 
 tasks {
--- src/main/java/com/jaycverg/svnstatusbarwidget/SvnRelativePathStatusBarWidget.java
+++ src/main/java/com/jaycverg/svnstatusbarwidget/SvnRelativePathStatusBarWidget.java
@@ -1,36 +1,19 @@
 package com.jaycverg.svnstatusbarwidget;
 
-import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
-
-import javax.swing.*;
-
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.svn.SvnVcs;
 
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.openapi.vfs.VirtualFileManagerListener;
-import com.intellij.openapi.wm.CustomStatusBarWidget;
 import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.impl.status.TextPanel;
-import com.intellij.ui.ClickListener;
+import com.intellij.openapi.wm.StatusBarWidget;
 
-public class SvnRelativePathStatusBarWidget extends TextPanel implements CustomStatusBarWidget {
+public class SvnRelativePathStatusBarWidget implements StatusBarWidget, StatusBarWidget.TextPresentation {
 
     public static final String ID = "svn-relative-path";
 
-    private String projectBasePath;
+    private StatusBar statusBar;
 
     public SvnRelativePathStatusBarWidget() {
-        new ClickListener() {
-            @Override
-            public boolean onClick(@NotNull MouseEvent mouseEvent, int i) {
-                setInfo();
-                return true;
-            }
-        }.installOn(this);
     }
 
     @NotNull
@@ -40,52 +23,38 @@ public class SvnRelativePathStatusBarWidget extends TextPanel implements CustomS
     }
 
     @Override
-    public JComponent getComponent() {
+    public void install(@NotNull StatusBar statusBar) {
+        this.statusBar = statusBar;
+        statusBar.updateWidget(ID());
+    }
+
+    @Nullable
+    @Override
+    public WidgetPresentation getPresentation() {
         return this;
     }
 
+    @NotNull
     @Override
-    public void install(@NotNull StatusBar statusBar) {
-        VirtualFileManager.getInstance().addVirtualFileManagerListener(new VirtualFileManagerListener() {
-            @Override
-            public void beforeRefreshStart(boolean asynchronous) {
-            }
-
-            @Override
-            public void afterRefreshFinish(boolean asynchronous) {
-                setInfo();
-            }
-        }, this);
-        projectBasePath = statusBar.getProject().getBasePath();
-        setInfo();
-    }
-
-    private void setInfo() {
-        var info = getSvnInfo(projectBasePath);
-        setText(info);
-    }
-
-    private String getSvnInfo(String basePath) {
-        try {
-            var projDir = Paths.get(basePath).toFile();
-            var proc = new ProcessBuilder("/opt/homebrew/bin/svn", "info")
-                    .directory(projDir)
-                    .start();
-
-            try (var reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
-                var info = reader.lines()
-                        .filter(s -> s.startsWith("Relative URL"))
-                        .findFirst()
-                        .orElse("")
-                        .split(":");
-                var path = info.length > 1 ? info[1].trim().substring(2) : "SVN Project";
-                return "[" + path + "]";
-            }
-        } catch (IOException e) {
-            System.out.println("Failed to get the SVN info. SVN cli may not be installed.");
-            e.printStackTrace();
+    public String getText() {
+        if (statusBar != null) {
+            var project = statusBar.getProject();
+            var svn = SvnVcs.getInstance(project);
+            var info = svn.getInfo(project.getBasePath());
+            var path = info.getUrl().toString().replace(info.getRepositoryRootUrl().toString(), "");
+            return "[" + path + "]";
         }
+        return "[Fetching...]";
+    }
 
-        return "";
+    @Override
+    public float getAlignment() {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public String getTooltipText() {
+        return getText();
     }
 }
