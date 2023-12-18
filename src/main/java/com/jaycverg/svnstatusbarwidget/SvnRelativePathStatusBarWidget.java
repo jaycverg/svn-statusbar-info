36044297790f3e7587aeb1099c1223f446edc3e2
jayrome <jaycverg@gmail.com>
2023-12-18 21:52:38 +0800

    set dependency to Subversion plugin

--- src/main/java/com/jaycverg/svnstatusbarwidget/SvnRelativePathStatusBarWidget.java
+++ src/main/java/com/jaycverg/svnstatusbarwidget/SvnRelativePathStatusBarWidget.java
@@ -1,19 +1,33 @@
 package com.jaycverg.svnstatusbarwidget;
 
+import java.awt.event.MouseEvent;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.svn.SvnVcs;
 
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.util.Consumer;
 
 public class SvnRelativePathStatusBarWidget implements StatusBarWidget, StatusBarWidget.TextPresentation {
 
     public static final String ID = "svn-relative-path";
+    private final ExecutorService exec = Executors.newSingleThreadExecutor();
 
     private StatusBar statusBar;
+    private String text = "Loading...";
 
-    public SvnRelativePathStatusBarWidget() {
+    @Nullable
+    @Override
+    public Consumer<MouseEvent> getClickConsumer() {
+        return (e) -> onClick();
+    }
+
+    private void onClick() {
+        statusBar.updateWidget(ID);
     }
 
     @NotNull
@@ -25,7 +39,27 @@ public class SvnRelativePathStatusBarWidget implements StatusBarWidget, StatusBa
     @Override
     public void install(@NotNull StatusBar statusBar) {
         this.statusBar = statusBar;
-        statusBar.updateWidget(ID());
+        setRelativePath();
+    }
+
+    private void setRelativePath() {
+        System.out.println("executor: " + exec);
+        exec.execute(() -> {
+            var project = statusBar.getProject();
+            if (project == null)
+                return;
+
+            var svn = SvnVcs.getInstance(project);
+            if (project.getBasePath() == null)
+                return;
+
+            var info = svn.getInfo(project.getBasePath());
+            if (info == null || info.getUrl() == null || info.getRepositoryRootUrl() == null)
+                return;
+
+            text = info.getUrl().toString().replace(info.getRepositoryRootUrl().toString(), "");
+            statusBar.updateWidget(ID());
+        });
     }
 
     @Nullable
@@ -37,14 +71,7 @@ public class SvnRelativePathStatusBarWidget implements StatusBarWidget, StatusBa
     @NotNull
     @Override
     public String getText() {
-        if (statusBar != null) {
-            var project = statusBar.getProject();
-            var svn = SvnVcs.getInstance(project);
-            var info = svn.getInfo(project.getBasePath());
-            var path = info.getUrl().toString().replace(info.getRepositoryRootUrl().toString(), "");
-            return "[" + path + "]";
-        }
-        return "[Fetching...]";
+        return text;
     }
 
     @Override
